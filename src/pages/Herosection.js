5ea5d290d864020c6f68ac871ecfff6ab79fde08
Shemonti Barua <shemonti33@gmail.com>
2025-06-14 14:42:05 +0600

    Updated portfolio with new section

--- src/pages/Herosection.js
+++ src/pages/Herosection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import HeroImg from '../assets/images/dia.png';
+import HeroImg from '../assets/images/dia2.png';
 import Button from './Button';
 import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
 import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
