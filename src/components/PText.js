d70c78e6b9738b5969daf9a363f914744221b828
Shemonti Barua <shemonti33@gmail.com>
2025-06-14 16:25:34 +0600

    Updated portfolio with new section

--- src/components/PText.js
+++ src/components/PText.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import styled from 'styled-components';
-
-// const PStyle = styled.div`
-//   max-width: 800px;
-//   margin: 0 auto;
-//   font-size: 1.8rem;
-//   line-height: 1.5em;
-//   @media only screen and (max-width: 768px) {
-//     font-size: 1.4rem;
-//   }
-// `;
-
-// export default function PText({ children }) {
-//   return (
-//     <PStyle className="para">
-//       <p>{children}</p>
-//     </PStyle>
-//   );
-// }
-// PText.js
 import React from 'react';
 import styled from 'styled-components';
 
@@ -27,15 +6,14 @@ const PStyle = styled.div`
   margin: 0 auto;
   font-size: 1.8rem;
   line-height: 1.5em;
-  color: ${(props) => props.color || 'var(--gray-1)'};
   @media only screen and (max-width: 768px) {
     font-size: 1.4rem;
   }
 `;
 
-export default function PText({ children, color }) {
+export default function PText({ children }) {
   return (
-    <PStyle className="para" color={color}>
+    <PStyle className="para">
       <p>{children}</p>
     </PStyle>
   );
